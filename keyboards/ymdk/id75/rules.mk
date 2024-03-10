d8df452ab82ba24f534414c970da80abeaf4711e
salmiron50 <smnam123@naver.com>
2024-03-10 19:36:15 +0900

    smnam map for ymdk id75

--- keyboards/ymdk/id75/rules.mk
+++ keyboards/ymdk/id75/rules.mk
@@ -4,3 +4,6 @@ MCU_LDSCRIPT = STM32F103xB
 # Wildcard to allow APM32 MCU
 DFU_SUFFIX_ARGS = -p FFFF -v FFFF
 
+
+VIA_ENABLE = yes			# Enable VIA
+KEY_LOCK_ENABLE = yes		# Enable KC_LOCK support (QK_LOCK)
