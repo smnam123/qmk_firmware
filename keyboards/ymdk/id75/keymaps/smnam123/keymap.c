31c8e7caf8e7057a8979136d583928ab49a99f32
salmiron50 <smnam123@naver.com>
2024-10-10 16:43:02 +0900

    add vial support

--- keyboards/ymdk/id75/keymaps/smnam123/keymap.c
+++ keyboards/ymdk/id75/keymaps/smnam123/keymap.c
@@ -37,28 +37,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Keymap _BASE: Base Layer (Default Layer)
     * ,--------------------------------------------------------------------------------------------------------.
-    * | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   \  | Bksp |
+    * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   -  |   =  |   \  | Bksp |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   7  |   8  |   9  |   Y  |   U  |   I  |   O  |   P  |  '   |
+    * | Tab  |   Q  |   W  |   E  |   R  |   T  |  F1  |  F2  |  F3  |   Y  |   U  |   I  |   O  |   P  |  "   |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-    * | LShft|   A  |   S  |   D  |   F  |   G  |   4  |   5  |   6  |   H  |   J  |   K  |   L  |   ;  |RShft |
+    * | Caps |   A  |   S  |   D  |   F  |   G  |  F4  |  F5  |  F6  |   H  |   J  |   K  |   L  |   ;  |RShft |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-    * | Caps |   Z  |   X  |   C  |   V  |   B  |   1  |   2  |   3  |   N  |   M  |   ,  |   .  |   ↑  |Enter |
+    * | LShft|   Z  |   X  |   C  |   V  |   B  |  F7  |  F8  |  F9  |   N  |   M  |   ,  |   .  |   ↑  |Enter |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-    * | LCtl |  Win | LAlt |   `  | LSpc |  Fn1 |   -  |   0  |   =  |   /  | RSpc | RAlt |   ←  |   ↓  |   →  |
+    * | LCtl |  Win | LAlt |   `  | LSpc |  Fn1 |  F10 |  F11 |  F12 |   /  | RSpc | RAlt |   ←  |   ↓  |   →  |
     * `--------------------------------------------------------------------------------------------------------'
     */
     [_BASE] = LAYOUT_ortho_5x15(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSLS, KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_7,    KC_8,    KC_9,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_4,    KC_5,    KC_6,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
-        KC_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_1,    KC_2,    KC_3,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_GRV,  KC_SPC,  MO(1),   KC_MINS, KC_0,    KC_EQL,  KC_SLSH, KC_SPC,  KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F1,   KC_F2,   KC_F3,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F4,   KC_F5,   KC_F6,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
+        KC_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F7,   KC_F8,   KC_F9,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_GRV,  KC_SPC,  MO(1),   KC_F10,  KC_F11,  KC_F12,  KC_SLSH, KC_SPC,  KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Keymap _FUNC: Function Layer
     * ,--------------------------------------------------------------------------------------------------------.
-    * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   -  |   =  |      |      |
+    * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
     * |      |      |   ↑  |      | PgUp |      |      |      |      |      |      |      |   [  |   ]  |      |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `--------------------------------------------------------------------------------------------------------'
     */
     [_FUNC] = LAYOUT_ortho_5x15(
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, KC_UP,   _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, MO(2),   _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_HOME, QK_LOCK, RGB_SPD, RGB_VAD, RGB_SPI, _______, _______, _______, _______, _______, _______,
