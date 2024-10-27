a11f2d9da02795ef5c10b8621c8dd1b8ca85fc1e
salmiron50 <smnam123@naver.com>
2024-10-27 12:20:50 +0900

    id75 key setting reformation

--- quantum/rgb_matrix/animations/pixel_rain_anim.h
+++ quantum/rgb_matrix/animations/pixel_rain_anim.h
@@ -6,24 +6,25 @@ RGB_MATRIX_EFFECT(PIXEL_RAIN)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 static bool PIXEL_RAIN(effect_params_t* params) {
-    static fast_timer_t timer = 0;
-    static uint16_t     index = 0;
-    static rgb_t        rgb   = {};
+    static uint32_t wait_timer = 0;
 
-    if (params->init) {
-        rgb_matrix_set_color_all(0, 0, 0);
+    inline uint32_t interval(void) {
+        return 500 / scale16by8(qadd8(rgb_matrix_config.speed, 16), 16);
     }
 
-    if (timer_elapsed_fast(timer) > (320 - rgb_matrix_config.speed)) {
-        hsv_t hsv = (random8() & 2) ? (hsv_t){0, 0, 0} : (hsv_t){random8(), random8_min_max(127, 255), rgb_matrix_config.hsv.v};
-        rgb       = rgb_matrix_hsv_to_rgb(hsv);
-        index     = random8_max(RGB_MATRIX_LED_COUNT);
-        timer     = timer_read_fast();
+    inline void rain_pixel(uint8_t led_index) {
+        if (!HAS_ANY_FLAGS(g_led_config.flags[led_index], params->flags)) {
+            return;
+        }
+        HSV hsv = (random8() & 2) ? (HSV){0, 0, 0} : (HSV){random8(), random8_min_max(127, 255), rgb_matrix_config.hsv.v};
+        RGB rgb = rgb_matrix_hsv_to_rgb(hsv);
+        rgb_matrix_set_color(led_index, rgb.r, rgb.g, rgb.b);
+        wait_timer = g_rgb_timer + interval();
     }
 
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
-    if (led_min <= index && index <= led_max && HAS_ANY_FLAGS(g_led_config.flags[index], params->flags)) {
-        rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+    if (g_rgb_timer > wait_timer) {
+        rain_pixel(random8_max(RGB_MATRIX_LED_COUNT));
     }
     return rgb_matrix_check_finished_leds(led_max);
 }
