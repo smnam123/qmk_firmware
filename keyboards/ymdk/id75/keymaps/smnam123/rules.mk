4c656d4546150d5a95c5106bb543ee7bbac909d8
salmiron50 <smnam123@naver.com>
2024-10-27 10:30:49 +0900

    Update Pixel Rain to respect LED range limits
    Improved effect with update to LED index within led_min and led_max limits
    & separate vial related config

--- keyboards/ymdk/id75/keymaps/smnam123/rules.mk
+++ keyboards/ymdk/id75/keymaps/smnam123/rules.mk
@@ -2,7 +2,5 @@
 # Change yes to no to disable
 
 VIA_ENABLE = yes			# Enable VIA
-VIAL_ENABLE = yes
-VIALRGB_ENABLE = yes
 LTO_ENABLE = yes 			# reduce firmware size
 KEY_LOCK_ENABLE = yes		# Enable KC_LOCK support (QK_LOCK)
